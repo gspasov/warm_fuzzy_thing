0f2eae36059139f9a13d3be93c48bb8763f018e1
Georgi Spasov <gvspasov@gmail.com>
2023-04-08 17:54:20 +0300

    feat: Fix spec types in Monad

--- lib/monad.ex
+++ lib/monad.ex
@@ -1,20 +1,19 @@
 defmodule Monad do
-  @spec maybe_to_either(maybe, reason) :: either
-        when maybe: nil | {:ok, value},
-             either: {:ok, value} | {:error, reason},
-             value: any(),
+  alias Monad.Either
+  alias Monad.Maybe
+
+  @spec maybe_to_either(Maybe.t(value), reason) :: Either.t(reason, value)
+        when value: any(),
              reason: any()
   def maybe_to_either(maybe, reason)
 
-  def maybe_to_either(:error, reason), do: {:error, reason}
+  def maybe_to_either(nil, reason), do: {:error, reason}
   def maybe_to_either({:ok, value}, _), do: {:ok, value}
 
-  @spec either_to_maybe(either) :: maybe
-        when maybe: nil | {:ok, value},
-             either: {:ok, value} | {:error, reason},
-             value: any(),
+  @spec either_to_maybe(Either.t(reason, value)) :: Maybe.t(value)
+        when value: any(),
              reason: any()
   def either_to_maybe(either)
-  def either_to_maybe({:error, _reason}), do: :error
+  def either_to_maybe({:error, _reason}), do: nil
   def either_to_maybe({:ok, value}), do: {:ok, value}
 end

658865542ea7512de51fe6d6e01c2dfc1b449367
Georgi Spasov <gvspasov@gmail.com>
2023-04-10 22:40:00 +0300

    fix: Fix link to source

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule WarmFuzzyThing.MixProject do
   use Mix.Project
 
   @version "0.1.0"
-  @source_url "https://github.com/gspasov/"
+  @source_url "https://github.com/gspasov/warm_fuzzy_thing"
   @authors ["Georgi Spasov"]
 
   def project do
